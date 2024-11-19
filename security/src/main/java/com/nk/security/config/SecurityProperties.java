3a78e7137fe453a2c23f451fbeca9a28ac90ddbc
Nalin Kumar <nalin12894+github@gmail.com>
2024-11-19 07:29:00 +0530

    security property bean to hold custom security property

---
+++ security/src/main/java/com/nk/security/config/SecurityProperties.java
@@ -0,0 +1,30 @@
+package com.nk.security.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Setter;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Configuration
+@ConfigurationProperties(prefix = "custom.security")
+@Setter
+public class SecurityProperties {
+
+    private String whitelistUrl;
+    private Map<String, String> roles; // <client_id, client_secret>
+
+    public Map<String, String> getRoles() {
+        if(roles != null) {
+          return  roles;
+        }
+        return Collections.emptyMap();
+    }
+
+    public String[] getRequestPermitAllPatterns() {
+        return whitelistUrl !=null ? whitelistUrl.split(",") : null;
+    }
+    
+}
