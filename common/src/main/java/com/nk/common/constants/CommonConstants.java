dcab0a409317478ed4d1286db3df8ce8ea4afaef
Nalin Kumar <nalin12894+github@gmail.com>
2024-11-19 07:24:40 +0530

    common consents

---
+++ common/src/main/java/com/nk/common/constants/CommonConstants.java
@@ -0,0 +1,9 @@
+package com.nk.common.constants;
+
+public class CommonConstants {
+    public static final String DATE_TIME_FORMATE = "yyyy-MM-dd HH:mm:ss";// 2019-12-14 19:10:04
+    public static final String DATE_FORMATE = "yyyy-MM-dd";
+    private CommonConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+}
