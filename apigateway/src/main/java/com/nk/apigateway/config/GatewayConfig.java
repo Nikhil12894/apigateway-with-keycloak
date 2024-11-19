b57fe5c8bacc504de0953194c2ce2cfb93005f95
Nalin Kumar <nalin12894+github@gmail.com>
2024-11-19 07:48:09 +0530

    added rout config for different service

---
+++ apigateway/src/main/java/com/nk/apigateway/config/GatewayConfig.java
@@ -0,0 +1,24 @@
+package com.nk.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+                .route("user-service-route", r -> r.path("/user/**")
+                        .filters(f -> f)
+                        .uri("http://localhost:8081/"))
+                .route("admin-service-route", r -> r.path("/admin/**")
+                        .filters(f -> f)
+                        .uri("http://localhost:8082/"))
+                .build();
+    }
+
+}
